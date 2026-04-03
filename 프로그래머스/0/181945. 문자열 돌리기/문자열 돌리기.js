05f0caccb283df7c5ce4ffecd1135b4e6a134377
usohee <rrwnsldj1004@gmail.com>
2026-04-03 15:28:23 +0900

    [level 0] Title: 문자열 돌리기, Time: 43.90 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    let str = input[0];
+    
+    for (let x of str) {
+        console.log(x);
+    }
+});
