a7519096c120bce1956d1d0a06767457aaac061a
usohee <rrwnsldj1004@gmail.com>
2026-04-03 15:27:36 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 45.23 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    const str = input[0];
+    let result ='';
+    
+    for (let x of str) {
+        if (x===x.toUpperCase()){
+            result+=x.toLowerCase();
+        }
+        else {
+            result+=x.toUpperCase();
+        }
+    }
+    console.log(result);
+});
